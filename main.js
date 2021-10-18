b8bbc4fbf9868ff4f8b42fc5f8245a0d569ce294
Ibrohim Rasulov <ibrohim05062000@gmail.com>
2021-10-18 22:52:21 +0500

    Create array for booklist and add event listener

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
 const bookList = [];
+
+const title = document.querySelector('#bookTitle');
+const author = document.querySelector('#bookAuthor');
+const btn = document.querySelector('#addBtn');
+const booklist = document.querySelector('ul');
+
+btn.addEventListener('click', function(){
+  const newBook = document.createElement('li');
+  const bookTitle = document.createElement('span');
+  const bookAuthor = document.createElement('span');
+  const deleteBtn = document.createElement('button');
+  
+  bookTitle.innerText = title;
+  console.log(bookTitle);
+})
