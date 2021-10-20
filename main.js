65ab41a78f6dcdee78b68a369d6ccee8cc937a24
Ibrohim Rasulov <ibrohim05062000@gmail.com>
2021-10-20 21:53:11 +0500

    Correct eslint errors

--- main.js
+++ main.js
@@ -1,34 +1,34 @@
 const list = document.querySelector('#list');
-const addBook = document.querySelector('#add-book');
 const form = document.querySelector('form');
 const title = document.querySelector('#book-title');
 const author = document.querySelector('#book-author');
 
-let bookArray = [];
+const bookArray = [];
 
-function Book (title, author){
+function Book(title, author) {
   this.title = title;
-this.author = author;
+  this.author = author;
 }
 
-function addToLocalStorage(){
-  let key = title.value;
-  localStorage.setItem(key, JSON.stringify(bookArray))
+function addToLocalStorage() {
+  const key = title.value;
+  localStorage.setItem(key, JSON.stringify(bookArray));
 }
 
-function addNewBook(){
-  let newBook = new Book(title.value, author.value)
+function addNewBook() {
+  const newBook = new Book(title.value, author.value);
   bookArray.push(newBook);
   addToLocalStorage();
 }
 
 form.addEventListener('submit', addNewBook);
 
-function showBook(){
+function showBook() {
   Object.keys(localStorage).forEach((key) => {
-    const dataFromLocalStorage = JSON.parse(localStorage.getItem(key))
-    if(localStorage){
-      dataFromLocalStorage.forEach(book => {
+    const dataFromLocalStorage = JSON.parse(localStorage.getItem(key));
+
+    if(localStorage) {
+      dataFromLocalStorage.forEach((book) => {
         const li = document.createElement('li');
         const bookTitle = document.createElement('span');
         const bookAuthor = document.createElement('span');
@@ -38,13 +38,13 @@ function showBook(){
         deleteBtn.classList.add('remove');
         bookTitle.textContent = book.title;
         bookAuthor.textContent = book.author;
-        
+
         deleteBtn.addEventListener('click', (e) => {
-          let key = book.title;
+          const key = book.title;
           localStorage.removeItem(key);
 
           e.target.parentNode.remove();
-        })
+        });
 
         li.classList.add('new-book');
         bookTitle.style.display = 'block';
