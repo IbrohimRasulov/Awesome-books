5d3f91b6fc8d727a0df6c1cfaf094f167178bd66
Ibrohim Rasulov <ibrohim05062000@gmail.com>
2021-10-21 21:47:56 +0500

    Add style of backgroun to each book

--- main.js
+++ main.js
@@ -57,7 +57,7 @@ function showBook() {
 
         deleteBtn.classList = 'remove btn btn-danger btn-sm float-end';
         list.classList = 'list-group';
-        li.classList = 'list-group-item list-group-item-light';
+        li.classList = 'list-group-item list-group-item-light odd';
 
         deleteBtn.addEventListener('click', (e) => {
           const bookToDelete = new Book(book.title, book.author);
